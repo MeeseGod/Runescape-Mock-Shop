5e35001e5ea6b00f9febf779835918cb94e818e6
JosephR <joseph_rundell@outlook.com>
2022-04-05 17:35:28 -0400

    Updated cart logic

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,42 +3,40 @@ import Navbar from "./Navbar";
 
 export default function Cart(props){
 
-  const [cartItems, setCartItems] = useState();
-  const [cartArray, setCartArray] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
   let keyCounter = 0;
 
-  function sortThroughItems(){
-    let counts = {};
-
+  function countItems(){
+    let countedItems = {};
     props.items.forEach(function(x){
-      counts[x.itemId] = (counts[x.itemId] || 0) + 1;
+      countedItems[x.itemId] = (countedItems[x.itemId] || 0) + 1;
       }
     );
-   setCartItems(counts)
-
+    updateCartArray(countedItems)
   }
 
-  function updateCartArray(){
-    // Get counts from sortThroughItems
-    // Split object into multiple objects via for each
-    // Place each object into array (each object contains item code and number)
-    // Retrieve item via item code from Inventory and display said item w/ the number of said items in cart
-    // ez pz
+  function updateCartArray(countedItems){
+    let newArray = [];
+    for(const property in countedItems){
+      let tempObject = {[property] : countedItems[property]}
+      newArray = [...newArray, tempObject]
+    }
+    setCartItems(newArray)
   }
 
   useEffect(() =>{
-      sortThroughItems()
-      updateCartArray()
+      countItems()
     }, []
   )
     return <div id="cartContainer">
         <Navbar/>
-        {props.items.map((item) => {
+        {cartItems.map((item) => {
           keyCounter = keyCounter + 1
           return <div className="cartItemDiv" key={keyCounter}> 
             {item.name}
             <button onClick={()=>{console.log(cartItems)}}>Ahhh</button>
           </div>
         })}
+        
     </div>
 }
