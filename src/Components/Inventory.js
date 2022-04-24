3483872e62eb698f2d5110a8e97c61ec984c90eb
JosephR <joseph_rundell@outlook.com>
2022-04-24 03:24:21 -0400

    Changed Shop styling

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -7,11 +7,46 @@ const Inventory = [
         itemId : "001"
     },
     {
+        name : "Rune Full Helm",
+        price: 20,
+        category : "armor",
+        image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
+        itemId : "002"
+    },
+    {
         name : "Rune Platebody",
         price: 30,
         category : "armor",
         image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
-        itemId : "002"
+        itemId : "003" 
+    },
+    {
+        name : "Rune Platelegs",
+        price: 25,
+        category : "armor",
+        image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
+        itemId : "004" 
+    },
+    {
+        name : "Rune Platelegs",
+        price: 25,
+        category : "armor",
+        image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
+        itemId : "005" 
+    },
+    {
+        name : "Rune Platelegs",
+        price: 25,
+        category : "armor",
+        image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
+        itemId : "006" 
+    },
+    {
+        name : "Rune Platelegs",
+        price: 25,
+        category : "armor",
+        image : "https://oldschool.runescape.wiki/images/thumb/Rune_platebody_detail.png/1200px-Rune_platebody_detail.png?3af70",
+        itemId : "007" 
     }
 ]
 
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import {Link} from "react-router-dom"
+import "../styles/Shop.css"
 
 export default function Shop(props){
 
@@ -13,17 +14,26 @@ export default function Shop(props){
 
   return(
       <div className="shopContainer">
-      <Navbar/>
-      <div className="shopItemsContainer">
-        {props.items.map((item) => {
-          return <div className="productDiv" key={item.name}>
-            <Link to={`/shop/${item.itemId}`}>
-            {item.name}
-            </Link> 
-            <button onClick={() => getItem(item)}>Add to Cart</button>
+        <Navbar/>
+        <div className="narrowSearchContainer">
+          <input type={"text"} className="narrowSearchInput"/>
+        </div>
+          <div className="shopItemDisplay">
+            {props.items.map((item) => {
+              return <div className="productDiv" key={item.name}>
+                <Link to={`/shop/${item.itemId}`} className="productLink">
+                  {/* <div className="shopImageContainer"> */}
+                    <img src={item.image} className="shopItemImage"/>
+                  {/* </div> */}
+                  <div className="productInfo">
+                    <div>{item.name}</div>
+                    <div>Price: {item.price}gp</div>
+                  </div>
+                </Link> 
+                <button onClick={() => getItem(item)}>Add to Cart</button>
+              </div>
+            })}
           </div>
-        })}
-      </div>
       </div>
   )
 }
